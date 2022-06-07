c9ef1b59f9ea1df4ffddd9c4fe783635c60f71f2
kouyate karim <karimkouyate@adjemin.com>
2022-06-07 16:20:37 +0000

    firebase functions

--- functions/index.js
+++ functions/index.js
@@ -7,6 +7,23 @@ const exports.randomNumber = functions.https.onRequest((request, response) => {
 
     res.send(number);
 })
+
+export const documentWriteListener = functions.firestore
+  .document('collection/{documentUid}')
+  .onWrite((change, context) => {
+
+    if (!change.before.exists) {
+      // New document Created : add one to count
+      db.doc(docRef).update({ numberOfDocs: FieldValue.increment(1) });
+    } else if (change.before.exists && change.after.exists) {
+      // Updating existing document : Do nothing
+    } else if (!change.after.exists) {
+      // Deleting document : subtract one from count
+      db.doc(docRef).update({ numberOfDocs: FieldValue.increment(-1) });
+    }
+
+    return;
+  });
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
